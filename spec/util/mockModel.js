d637e7ffe4c96ae3590641aa324e2810a788de90
Chris Jensen <chris@broadthought.co>
2017-11-16 21:01:25 +0800

    Add mock destroy method to MockModel

--- spec/util/mockModel.js
+++ spec/util/mockModel.js
@@ -1,8 +1,12 @@
+async function noop() {};
+
 function MockModel(name, dummyRecord) {
 	const mock = {
 		findAll: async () => [dummyRecord],
-		findOne: async () => dummyRecord,
+		findOne: async () => Object.assign({ destroy: noop }, dummyRecord),
 		create: async () => dummyRecord,
+		destroy: async () => undefined,
+		update: async () => dummyRecord,
 	};
 
 	return mock;
