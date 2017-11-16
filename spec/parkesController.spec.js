3083e39c048e8e72a2965c7b5c902f0262538056
Chris Jensen <chris@broadthought.co>
2017-11-16 21:02:29 +0800

    Fix up some broken tests

--- spec/parkesController.spec.js
+++ spec/parkesController.spec.js
@@ -2,21 +2,32 @@ const sinon = require('sinon');
 const MockModel = require('./util/mockModel');
 const ParkesController = require('../index.js');
 const chai = require('chai');
+const chaiSubset = require('chai-subset');
 const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
 
 chai.use(sinonChai);
+chai.use(chaiSubset);
+chai.use(chaiAsPromised);
 
 const { mockKoaContext } = require('./util/mockKoa');
 
 const { expect } = chai;
 
+const sandbox = sinon.createSandbox();
+
 const dummyRecord = { id: 1, name: 'Harvey Milk' };
+const dummyPost = { job: 'Mayor' };
 // eslint-disable-next-line no-empty-function
 async function noop() {}
 
 const authObj = {
-	authorize: function authorize() {},
+	authorize: function nestedAuth() {},
 };
+authObj.authorize.original = true;
+function authorize(...param) {
+	authObj.authorize(...param);
+}
 
 class UserController extends ParkesController {}
 
@@ -25,12 +36,14 @@ describe('restController', () => {
 	let models;
 	let authSpy;
 	let ctx;
-	let authorize;
 
-	function basicRequest(action) {
+	function basicRequest(action, body) {
 		before(async () => {
-			ctx = mockKoaContext();
+			ctx = mockKoaContext({ body });
 			await userController[action](ctx, noop);
+		});
+		after(() => {
+			sandbox.reset();
 		});
 	}
 
@@ -44,7 +57,8 @@ describe('restController', () => {
 
 	function itAuthorizesAgainstRecord(action) {
 		it('calls authorize', async () => {
-			expect(authSpy).to.have.been.calledOnce();
+			// eslint-disable-next-line no-unused-expressions
+			expect(authSpy).to.have.been.calledOnce;
 			expect(authSpy.getCall(0).args[0]).to.eq(ctx);
 			// First call is with the class
 			expect(authSpy.getCall(0).args[1]).to.containSubset({ action, model: dummyRecord });
@@ -53,7 +67,19 @@ describe('restController', () => {
 
 	function itAssignsRecordToStateData() {
 		it('assigns record to state.data', async () => {
-			expect(ctx.state.data).to.eq(dummyRecord);
+			expect(ctx.state.data).to.containSubset(dummyRecord);
+		});
+	}
+
+	function itRejectsRestrictedKeys(action) {
+		it('rejects restricted keys', async () => {
+			const badBody = {
+				id: 1,
+				uuid: 'new-uuid',
+			};
+			const message = 'You may not update the fields: id, uuid';
+			ctx = mockKoaContext({ body: badBody });
+			await expect(userController[action](ctx, noop)).to.be.rejectedWith(message);
 		});
 	}
 
@@ -62,9 +88,7 @@ describe('restController', () => {
 			User: MockModel('User', dummyRecord),
 		};
 
-		authSpy = sinon.stub(authObj, 'authorize');
-		// eslint-disable-next-line prefer-destructuring
-		authorize = authObj.authorize;
+		authSpy = sandbox.spy(authObj, 'authorize');
 
 		userController = new UserController('user', { models, authorize });
 	});
@@ -97,23 +121,32 @@ describe('restController', () => {
 	});
 
 	describe('create', () => {
-		before(async () => {
-			ctx = mockKoaContext();
-			await userController.create(ctx, noop);
+		context('When request is simple and correct', () => {
+			basicRequest('create', dummyPost);
+			itAuthorizesAgainstModel();
+			itAssignsRecordToStateData();
 		});
-		itAuthorizesAgainstModel();
-		itAssignsRecordToStateData();
+
+		context('When request tries to update restricted keys', () => {
+			itRejectsRestrictedKeys('create');
+		});
 	});
 
 	describe('update', () => {
-		basicRequest('update');
-		itAuthorizesAgainstRecord('update');
-		itAssignsRecordToStateData();
+		context('When request is simple and correct', () => {
+			basicRequest('update', dummyPost);
+			itAuthorizesAgainstRecord('update');
+			itAssignsRecordToStateData();
+		});
+
+		context('When request tries to update restricted keys', () => {
+			itRejectsRestrictedKeys('update');
+		});
 	});
 
 	describe('destroy', () => {
-		basicRequest('update');
-		itAuthorizesAgainstRecord('update');
+		basicRequest('destroy');
+		itAuthorizesAgainstRecord('destroy');
 		itAssignsRecordToStateData();
 	});
 
