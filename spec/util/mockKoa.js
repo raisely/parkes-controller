621f6bd9f49561643bdbb75ea1374607e5ab4d96
Chris Jensen <chris@broadthought.co>
2017-11-16 21:01:06 +0800

    Add support for mocking state and body on request

--- spec/util/mockKoa.js
+++ spec/util/mockKoa.js
@@ -1,9 +1,17 @@
 const _ = require('lodash');
 
 function mockKoaContext(options) {
+	// eslint-disable-next-line no-param-reassign
+	options = options || {};
 	const ctx = _.defaults(options, {
 		query: {},
 		params: {},
+		state: {},
+		request: {
+			body: {
+				data: options.body || {},
+			},
+		},
 	});
 
 	return ctx;
