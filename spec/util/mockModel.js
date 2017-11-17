37823c0fe5ba710e6980d350983acc19ab060663
Chris Jensen <chris@broadthought.co>
2017-11-17 11:25:39 +0800

    Have update mock return self

--- spec/util/mockModel.js
+++ spec/util/mockModel.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-empty-function
 async function noop() {}
+async function returnSelf() { return this; }
 
 function MockModel(name, dummyRecord) {
 	const mock = {
 		findAll: async () => [dummyRecord],
 		findAndCountAll: async () => ({ count: 1, rows: [dummyRecord] }),
-		findOne: async () => Object.assign({ destroy: noop, update: noop }, dummyRecord),
+		findOne: async () => Object.assign({ destroy: noop, update: returnSelf }, dummyRecord),
 		create: async () => dummyRecord,
 		destroy: async () => undefined,
 		update: async () => dummyRecord,
